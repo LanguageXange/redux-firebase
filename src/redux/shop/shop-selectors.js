96513fbd9e30e4f5f10b39640a12b1586a11bcbc
LanguageXange <hlin0017@seattlecentral.edu>
2021-10-22 15:22:00 -0700

    update shop data to object and update shop selector

--- src/redux/shop/shop-selectors.js
+++ src/redux/shop/shop-selectors.js
@@ -2,24 +2,14 @@ import { createSelector } from "reselect";
 import memoize from "lodash.memoize";
 const selectShop = (state) => state.myshop;
 
-// create mapping string to id map -> temporary solution - this map will be removed after moving data to firebase
-const map = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5,
-};
 export const selectShopCollections = createSelector(
   selectShop,
   (s) => s.collections
 );
-
-// need to memoize entire fn
-// Memoize does the same idea of memoization as reselect does for our selectors,
-// except this time we're memoizing the return of our function which returns our selector:
+export const selectCollectionsForPreview = createSelector(
+  selectShopCollections,
+  (collections) => Object.keys(collections).map((item) => collections[item])
+);
 export const selectShopItem = memoize((param) =>
-  createSelector(selectShopCollections, (collections) =>
-    collections.find((collection) => collection.id === map[param])
-  )
+  createSelector(selectShopCollections, (collections) => collections[param])
 );
